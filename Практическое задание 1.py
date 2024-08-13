9790e305a0626e113c7baa239b2c337e3f4c5797
Pavel100889 <ermak264_89@mail.ru>
2024-08-13 13:44:36 +0300

    Add files via upload

---
+++ Практическое задание 1.py
@@ -0,0 +1,13 @@
+example = "Практическое задание 1"
+print(example[0])
+print(example[-1])
+len(example)
+V=len(example)
+B=V//2
+if(B)==0:
+    C=B-1
+else:
+    C=B
+print(example[C:])
+print(example[::-1])
+print(example[::2])
