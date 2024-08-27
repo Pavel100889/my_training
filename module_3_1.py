f2d8168a89be94dd7e4d7b529af72943fc958792
ermak264_89@mail.ru <ermak264_89@mail.ru>
2024-08-27 11:31:08 +0300

    26.08.24

---
+++ module_3_1.py
@@ -0,0 +1,20 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    print(len(string), string.upper(), string.lower())
+    count_calls()
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.upper()
+    list_to_search_up = [s.upper() for s in list_to_search]
+    if string in list_to_search_up:
+        print(True)
+    else:
+        print(False)
+string_info('Alcatel')
+string_info('Heroes')
+is_contains('Laptop',['laptip', 'LApTOP','laltap'])
+is_contains('Modem', ['madem', 'Mdem', 'MIDEM'])
+print(calls)
