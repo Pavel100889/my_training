ec19871b00e12195fa0585fc5c95c30b4c370503
ermak264_89@mail.ru <ermak264_89@mail.ru>
2024-09-10 13:30:33 +0300

    27.08.24

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from module_4_1_fake_math import divide as fake_divide
+from module_4_1_true_math import divide as true_divide
+
+print(true_divide(15, 0))
+print(true_divide(15, 5))
+print(fake_divide(15, 0))
+print(fake_divide(15, 5))
