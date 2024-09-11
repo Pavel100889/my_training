05ae561d09c2f96e69eae1c12bcd7be6bd12757e
ermak264_89@mail.ru <ermak264_89@mail.ru>
2024-09-11 17:21:13 +0300

    11.09.24

--- module_5_1.py
+++ module_5_1.py
@@ -6,7 +6,11 @@ class House:
     def go_to(self, new_floor):
             if new_floor > self.number_of_floors or new_floor < 1:
                 print("Такого этажа не существует")
-
+            else:
+                for i in range (1, new_floor+1):
+                    print (i)
 
 h1 = House('Москва', 20)
-h1.go_to(30)
+h2 = House('Владикавказ', 5)
+h1.go_to(10)
+h2.go_to(8)
